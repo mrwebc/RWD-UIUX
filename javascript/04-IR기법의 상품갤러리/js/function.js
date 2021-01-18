5f362f9c0136b5a5017a1145af8b826164cada9c
안쌤 <mrwebc@gmail.com>
2021-01-18 13:21:11 +0900

    04

--- javascript/04-IR기법의 상품갤러리/js/function.js
+++ javascript/04-IR기법의 상품갤러리/js/function.js
@@ -1,1 +1,24 @@
 //준비핸들러
+$(document).ready(function(){
+
+    /**
+     * 알고리즘
+     * 
+     * 1. 썸네일(a)에 대한 click이벤트 구문 작성
+     * 2. a태그의 기본기능 작동 금지
+     * 3. 이번에 클릭한 요소의 href 속성값을 가져온다.
+     * 4. p의 배경이미지를 변경한다.
+     */
+
+     const $thmbs = $(".thmbs a");
+
+     $thmbs.on('click', function(evt){
+        evt.preventDefault();
+
+        const imgSrc = $(this).attr('href');
+
+        $("p").css({
+            'background-image' : 'url(' + imgSrc + ')'
+        });
+     });
+});
