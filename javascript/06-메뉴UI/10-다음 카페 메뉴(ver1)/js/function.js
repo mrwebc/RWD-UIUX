59ef68ffc0f3aef4ca5003ae912bd54130b8c332
안쌤 <mrwebc@gmail.com>
2021-01-20 11:21:07 +0900

    06-10

--- javascript/06-메뉴UI/10-다음 카페 메뉴(ver1)/js/function.js
+++ javascript/06-메뉴UI/10-다음 카페 메뉴(ver1)/js/function.js
@@ -1,1 +1,16 @@
-$
+$(function(){
+
+    const $mnu = $('header>nav>.gnb>li>a');
+
+    $mnu.on('click', function(evt){
+        evt.preventDefault;
+
+        $(this).parent().addClass('on');
+        $(this).parent().siblings().removeClass('on');
+
+        //위의 두 문장을 합침
+        $(this).parent().addClass('on').siblings().removeClass('on');
+    });
+
+
+});
