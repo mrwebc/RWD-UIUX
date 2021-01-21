817a12d421ee7d114a654f96b8f508b107e3cb7f
안쌤 <mrwebc@gmail.com>
2021-01-21 13:16:33 +0900

    07

--- javascript/07-슬라이드/10-헤어 슬라이드/js/function.js
+++ javascript/07-슬라이드/10-헤어 슬라이드/js/function.js
@@ -1,1 +1,20 @@
-$
+$(function(){
+
+    //하단 인디케이터
+    const $indicator = $(".slides>.slides-pagination>li>a");
+
+    //인디케이터에 대한 클릭이벤트 구문 
+    $indicator.on('click', function(evt){
+
+        //이번에 클릭한 인디케이터의 인덱스번호 추출
+        const nowIdx = $indicator.index(this);//0~4값
+
+        //헤어컨테이너의 left 좌표값 변경
+        $(".slides>.slides-hair").css({
+            left : -250*nowIdx
+        });
+
+        //앵커태그의 기본기능 작동 방지
+        evt.preventDefault();
+    })
+});
