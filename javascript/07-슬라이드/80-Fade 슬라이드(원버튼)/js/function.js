a8bd99eabddc1d3e3052ee0851487d3769471438
안쌤 <mrwebc@gmail.com>
2021-02-05 15:43:40 +0900

    80

--- javascript/07-슬라이드/80-Fade 슬라이드(원버튼)/js/function.js
+++ javascript/07-슬라이드/80-Fade 슬라이드(원버튼)/js/function.js
@@ -5,25 +5,63 @@ $(function(){
 
     const $indicator = $("#slides>.slides-pagination>li>a");
 
-    //인디케이터에 대한 클릭이벤트 구문을 작성해야 한다.
-    $indicator.on('click',function(evt){
-        evt.preventDefault();
+
+    //공통부분 함수처리
+    const fadeActionFn = function(){
+        //슬라이드 페이드 처리
+        //1)이전 슬라이드는 사라지는 코드
+        $("#slides>.slides-container>li").eq(oldIdx).stop().fadeOut(1000);
+
+        //2)이번에 나타날 슬라이드에 대한 코드
+        $("#slides>.slides-container>li").eq(nowIdx).stop().fadeIn(1000);
 
         //인디케이터 활성화 구문을 작성해야 한다.
         //그렇다면 이걸 하는데 필요한 건 뭐가 있을까?
-        $(this).parent().addClass('on').siblings().removeClass('on');
+        $indicator.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+    };
 
+    //인디케이터에 대한 클릭이벤트 구문을 작성해야 한다.
+    $indicator.on('click',function(evt){
+        evt.preventDefault();
 
         oldIdx = nowIdx;//인덱스 값 보존
         nowIdx = $indicator.index(this);
 
 
-        //슬라이드 페이드 처리
-        //1)이전 슬라이드는 사라지는 코드
-        $("#slides>.slides-container>li").eq(oldIdx).fadeOut(1000);
+        fadeActionFn();
+    });
 
-        //2)이번에 나타날 슬라이드에 대한 코드
-        $("#slides>.slides-container>li").eq(nowIdx).fadeIn(1000);
+
+    //이전
+    $(".slides-prev").on('click', function(evt){
+        evt.preventDefault();
+
+        oldIdx = nowIdx;//인덱스 값 보존
+
+        //인덱스 추출
+        if(nowIdx>0){
+            nowIdx--;
+        }else{
+            nowIdx = 3;
+        }
+
+        fadeActionFn();
+    });
+
+    //다음
+    $(".slides-next").on('click', function(evt){
+        evt.preventDefault();
+
+        oldIdx = nowIdx;//인덱스 값 보존
+
+        //인덱스 추출
+        if(nowIdx<3){
+            nowIdx++;
+        }else{
+            nowIdx = 0;
+        }
+
+        fadeActionFn();
     });
 
 });
